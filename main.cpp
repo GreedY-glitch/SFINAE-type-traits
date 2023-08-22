77b6940869568cd88dac0f53b61741b64d921e15
GreedY-glitch <dnspopagat@gmail.com>
2023-08-22 11:28:27 +0800

    Some changes

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 
 
 
+const int foo(int, int) {}
+
 auto main(int argc, char** argv) -> int
 {
     f(1, 2);
+
+    const int&& x = foo(1, 2);
+    decltype(auto) y = foo(1, 2);
+
+    std::cout << y << std::endl;
     return EXIT_SUCCESS;
 }
